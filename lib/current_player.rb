70260f02a301dd24e3bef6313ea0183faa919036
Sawandi Wilson <sawandiwilson@gmail.com>
2020-06-21 13:49:22 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,2 +1,20 @@
-def turn_count(board, array)
+require 'pry'
+
+def turn_count(board)
+  counter = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    binding.pry
+    return "X"
+  else
+    return "O"
+  end
 end
